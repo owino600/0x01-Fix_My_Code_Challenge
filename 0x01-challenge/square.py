93170f760eae107c224a9dc4a52acc29f502daf5
owino600 <owinodaniel600@gmail.com>
2024-02-10 12:23:31 +0300

    square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -14,7 +14,7 @@ class Square():
         """ Area of the square """
         return self.width * self.height
 
-    def Perimeter_Of_My_Square(self):
+    def perimeter_of_my_square(self):
         return (self.width * 4)
 
     def __str__(self):
@@ -25,4 +25,4 @@ if __name__ == "__main__":
     s = Square(width=12, height=12)
     print(s)
     print(s.area_of_my_square())
-    print(s.Perimeter_Of_My_Square())
+    print(s.perimeter_of_my_square())
